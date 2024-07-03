dd94fb9323b007990ca558232ab7ea34e2a70b18
Mickeyb12 <michelbbiney12@gmail.com>
2024-07-03 15:12:03 +0000

    CartContext.js

--- FashionFusion/CartContext.js
+++ FashionFusion/CartContext.js
@@ -1,16 +1,44 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
+  useEffect(() => {
+    const loadCart = async () => {
+      try {
+        const savedCart = await AsyncStorage.getItem('cart');
+        if (savedCart !== null) {
+          setCart(JSON.parse(savedCart));
+        }
+      } catch (error) {
+        console.error('Failed to load cart from storage', error);
+      }
+    };
+
+    loadCart();
+  }, []);
+
+  const saveCart = async (cart) => {
+    try {
+      await AsyncStorage.setItem('cart', JSON.stringify(cart));
+    } catch (error) {
+      console.error('Failed to save cart to storage', error);
+    }
+  };
+
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
+    const updatedCart = [...cart, product];
+    setCart(updatedCart);
+    saveCart(updatedCart);
   };
 
   const removeFromCart = (product) => {
-    setCart((prevCart) => prevCart.filter(item => item.title !== product.title));
+    const updatedCart = cart.filter(item => item.title !== product.title);
+    setCart(updatedCart);
+    saveCart(updatedCart);
   };
 
   return (
